169658948f02d8fd639f04fd4030a9a5c842efc1
lale <liyeva820@gmail.com>
2025-06-16 00:45:15 +0400

    changed title

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,9 +13,9 @@ function Main({ selectedRegion, data }) {
       <section className="dark:bg-gray-900 bg-purple-50 dark:text-gray-50">
         { !selectedRegion && <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
           <h1 className="text-4xl font-bold leading-none sm:text-5xl">
-            Quisquam necessita vel
-            <span className="dark:text-violet-600">laborum doloribus</span>
-            delectus
+            Discover the World: 
+            <span className="dark:text-violet-600">All Countries </span>
+            in One Place
           </h1>
           <input
             onChange={(e) => handleInput(e)}
